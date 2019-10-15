3854c917151772117ec55217c571e78af6e85200
Ankur Tripathi <ankur456799@gmail.com>
2019-10-15 18:47:27 +0530

    Add project files.

---
+++ DialogTest/View/DialogTest.xaml.cs
@@ -0,0 +1,47 @@
+﻿using DialogTest.Constants;
+using DialogTest.ViewModel;
+using GalaSoft.MvvmLight.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DialogTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class DialogTest : Window
+    {
+        public DialogTestViewModel ViewModel = new DialogTestViewModel();
+        public DialogTest()
+        {
+            InitializeComponent();
+            InitMessenger();
+            this.DataContext = ViewModel;
+        }
+
+        private void InitMessenger()
+        {
+            Messenger.Default.Unregister<bool>(this,
+                    MessengerToken.CLOSEWINDOW, CloseWindow);
+            Messenger.Default.Register<bool>(this,
+                MessengerToken.CLOSEWINDOW, CloseWindow);
+        }
+
+        private void CloseWindow(bool obj)
+        {
+            this.Close();
+        }
+    }
+}
