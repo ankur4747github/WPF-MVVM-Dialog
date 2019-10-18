53557504a14f007917c96f98b869b0f8fa408c05
Ankur Tripathi <ankur456799@gmail.com>
2019-10-18 18:46:05 +0530

    Added TEst Data

--- Test/MainWindow.xaml.cs
+++ Test/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using DialogBeamProperties.Model;
 using System;
+using System.Collections.Generic;
 using System.Windows;
 
 namespace Test
@@ -9,13 +10,43 @@ namespace Test
     /// </summary>
     public partial class MainWindow : Window, IDisposable
     {
+        DialogBeamProperties.DialogBeamProperties test;
         public MainWindow()
         {
             InitializeComponent();
             IProperties prop = new DialogBeamProperties.Model.Properties();
-            DialogBeamProperties.DialogBeamProperties test = new DialogBeamProperties.DialogBeamProperties(prop);
+            SetTestDataIntoProperties(ref prop);
+            test = new DialogBeamProperties.DialogBeamProperties(prop);
+            test.Closing += Test_Closing;
             test.Show();
             this.Hide();
+        }
+
+        private void Test_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            IProperties prop  = test.GetPropertiesData();
+        }
+
+        private void SetTestDataIntoProperties(ref IProperties prop)
+        {
+            List<string> list = new List<string>() { "a","b","c","d"};
+
+            prop.LoadDataComboBox = list;
+            prop.SelectedDataInLoadDataComboBox = list[0];
+            prop.IsAttributesProfileChecked = true;
+            prop.AttributesProfileText = "H100";
+
+            List<string> onPlaneList = new List<string>() { "Middle", "Right", "Left" };
+            prop.PositionOnPlaneComboBox = onPlaneList;
+            prop.SelectedDataInPositionOnPlaneComboBox = onPlaneList[0];
+
+            List<string> rotationList = new List<string>() { "Front", "Top", "Back","Below" };
+            prop.PositionRotationComboBox = rotationList;
+            prop.SelectedDataInPositionRotationComboBox = rotationList[0];
+
+            List<string> atDepthList = new List<string>() {"Middle", "Front","Behind" };
+            prop.PositionAtDepthComboBox = atDepthList;
+            prop.SelectedDataInPositionAtDepthComboBox = atDepthList[0];
         }
 
         public void Dispose()
