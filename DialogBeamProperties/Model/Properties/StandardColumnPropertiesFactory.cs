9e9f052b3729a55129030419af91742e817a1f04
BK <benkoshy@hotmail.com>
2019-10-23 09:54:38 +1100

    Add: factories for creating BeamProperties and ColumnProperties

---
+++ DialogBeamProperties/Model/Properties/StandardColumnPropertiesFactory.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DialogBeamProperties.Model.Properties
+{
+    public class StandardColumnPropertiesFactory
+    {
+        public IColumnProperties CreateStandardProperties()
+        {
+            IColumnProperties columnProperties = new ColumnProperties();
+
+            List<string> list = new List<string>() { "a", "b", "c", "d" };
+
+            columnProperties.LoadDataComboBox = list;
+            columnProperties.SelectedDataInLoadDataComboBox = list[0];
+            columnProperties.IsAttributesProfileChecked = true;
+            columnProperties.AttributesProfileText = "H100";
+
+            List<string> verticleList = new List<string>() { "Middle", "Right", "Left" };
+            columnProperties.PositionVerticalComboBox = verticleList;
+            columnProperties.SelectedDataInPositionVerticalComboBox = verticleList[0];
+
+            List<string> rotationList = new List<string>() { "Front", "Top", "Back", "Below" };
+            columnProperties.PositionRotationComboBox = rotationList;
+            columnProperties.SelectedDataInPositionRotationComboBox = rotationList[0];
+
+            List<string> horizontalList = new List<string>() { "Middle", "Front", "Behind" };
+            columnProperties.PositionHorizontalComboBox = horizontalList;
+            columnProperties.SelectedDataInPositionHorizontalComboBox = horizontalList[0];
+
+            columnProperties.IsNumberingSeriesPartPrefixChecked = true;
+            columnProperties.NumberingSeriesPartPrefixText = "";
+            columnProperties.IsNumberingSeriesPartStartumberChecked = true;
+            columnProperties.NumberingSeriesPartStartNumberText = "";
+            columnProperties.IsNumberingSeriesAssemblyPrefixChecked = true;
+            columnProperties.NumberingSeriesAssemblyPrefixText = "";
+            columnProperties.IsNumberingSeriesAssemblyStartumberChecked = true;
+            columnProperties.NumberingSeriesAssemblyStartNumberText = "";
+            columnProperties.IsAttributesNameChecked = true;
+            columnProperties.AttributesNameText = "";
+            columnProperties.IsAttributesProfileChecked = true;
+            columnProperties.AttributesProfileText = "";
+            columnProperties.IsAttributesMaterialChecked = true;
+            columnProperties.AttributesMaterialText = "";
+            columnProperties.IsAttributesFinishChecked = true;
+            columnProperties.AttributesFinishText = "";
+            columnProperties.IsAttributesClassChecked = true;
+            columnProperties.AttributesClassText = "";
+            columnProperties.PositionVerticalText = 0;
+            columnProperties.PositionRotationText = 0;
+            columnProperties.PositionHorizontalText = 0;
+            columnProperties.IsPositionLevelsTopChecked = true;
+            columnProperties.PositionLevelsTopText = 0;
+            columnProperties.IsPositionLevelsBottomChecked = true;
+            columnProperties.PositionLevelsBottomText = 0;
+
+            return columnProperties;
+        }
+    }
+}
