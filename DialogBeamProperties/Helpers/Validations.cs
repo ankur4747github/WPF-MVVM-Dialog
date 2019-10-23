1705b47de77e647bc226c918e84ab8a440d961e1
BK <benkoshy@hotmail.com>
2019-10-24 00:51:57 +1100

    note to ankur:  please encapsulate all validation logic in one class

--- DialogBeamProperties/Helpers/Validations.cs
+++ DialogBeamProperties/Helpers/Validations.cs
@@ -9,8 +9,49 @@ using static DialogBeamProperties.Model.ProfileFileData.ProfileFileData;
 
 namespace DialogBeamProperties.Helpers
 {
-    public class Validations
+    // Hi Ankur
+    // Right now the validation logic is woven into the view model classes.
+    // I cannot use your Validations class right now, without also
+    // Loading all the profile strings by myself. The profile strings are loaded
+    // from within the view models classes. I don't want to have to write
+    // some code again to validate this.
+
+    // What I want to be able to do in my code is something like this.
+
+    // IColumnProperties columnproperties = ....
+    // if (validator.IsValid(iColumnProperties))
+    //{
+    //     if it is valid then I continue as before.
+    //}
+    //else
+    //{
+    //     otherwise I take some corrective action
+    //}
+
+    // IBeamProperties beamProperties = ....
+    // if (validator.IsValid(iColumnProperties))
+    //{
+    //     if it is valid then I continue as before.
+    //}
+    // I will otherwise have to load the profiles and instantiate them myself.
+    // The profile data right now is loaded from within the view model.
+
+    public class Validator
     {
+        // Data must be preloaded for this to work.
+        // You could possibly store this is a static variable, to
+        // prevent the reloading of already existing data?
+
+        //public bool IsValid(IColumnProperties columnProperties)
+        //{
+        //    return IsValidProfile(columnProperties.AttributesProfileText) && AreTopAndBottomPositionsValid(columnProperties.PositionLevelsTopText, columnProperties.PositionLevelsBottomText);
+        //}
+
+        //public bool IsValid(IBeamProperties beamProperties)
+        //{
+        //    return IsValidProfile(beamProperties.AttributesProfileText);
+        //}
+
         public bool IsValidProfile(string attributesProfileText, List<ProfileData> profiles)
         {
             try
